9315376fdb984da7e8a23c0a39c8b934ce5f0cf1
voidnowhere <alivnw@outlook.com>
2024-01-02 13:36:36 +0100

    starter config

---
+++ backend/gateway-service/src/main/java/com/example/gatewayservice/config/GatewayConfig.java
@@ -0,0 +1,21 @@
+package com.example.gatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-service", r -> r
+                        .path("/api/auth/**")
+                        .uri("lb://auth-service")
+                ).route("trip-service", r -> r
+                        .path("/api/trips/**")
+                        .uri("lb://trip-service")
+                ).build();
+    }
+}
